83b5cd68bf64e116443bee09b825b96c63eedb67
xhjiang <3356569649@qq.com>
2022-09-28 19:51:09 +0800

    AIlearning的python基础学习

---
+++ src/py3.x/ml/2.KNN/local.py
@@ -0,0 +1,5 @@
+def init():
+    # int a=10
+    print('hello,ZHQ');
+if __name__ == '__main__':
+    init()
--- src/py3.x/ml/2.KNN/sklearn-knn-test.py
+++ src/py3.x/ml/2.KNN/sklearn-knn-test.py
@@ -25,17 +25,16 @@ n_neighbors = 3
 
 #  导入一些要玩的数据(鸢尾花数据集)
 iris = datasets.load_iris()
-X = iris.data[:, :2]  # 我们只采用前两个feature. 我们可以使用二维数据集避免这个丑陋的切片
+X = iris.data[:, :2]  # 我们只采用前两个feature. 我们可以使用二维数据集避免这个丑陋的切片（第一个表示要多少个数据，第二个表示要几维的数据）
+#第一个表示要多少条，第二个表示要多少列
 y = iris.target
 
-# print 'X=', type(X), X
-# print 'y=', type(y), y
+# print ('X=', type(X))
+# print(X)
+# print(iris.feature_names)
+# print(len(X))
+# print ('y=', type(y), len(y),y)
 
-# X = array([[-1.0, -1.1], [-1.0, -1.0], [0, 0], [1.0, 1.1], [2.0, 2.0], [2.0, 2.1]])
-# y = array([0, 0, 0, 1, 1, 1])
-
-# print 'X=', type(X), X
-# print 'y=', type(y), y
 
 h = .02  # 网格中的步长
 
@@ -43,11 +42,9 @@ h = .02  # 网格中的步长
 cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])#点
 cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])#区域色块
 
-# cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA'])
-# cmap_bold = ListedColormap(['#FF0000', '#00FF00'])
-
 for weights in ['uniform', 'distance']:
     # 我们创建了一个knn分类器的实例，并拟合数据。
+    # 默认情况下kneighbors查询使用的邻居数。就是k-NN的k的值n_neighbors，选取最近的k个点，未指定的话默认值为5
     clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
     clf.fit(X, y)
 
@@ -55,6 +52,8 @@ for weights in ['uniform', 'distance']:
     # 来绘制网格中的点 [x_min, x_max]x[y_min, y_max].
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    # 在给定间隔内返回均匀间隔的值
+    # 值在半开区间 [开始，停止]内生成（换句话说，包括开始但不包括停止的区间）,返回的是 ndarray
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                          np.arange(y_min, y_max, h))
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
